112e9ba5b2a5145d3e05b8a68c02af61f8899f11
z5022859 <z5022859@cse.unsw.edu.au>
2017-09-27 08:40:49 +1000

    I have the skeleton for Task 1 done but I don't think the wInFactor and wOutFactor are being calculated correctly

--- pagerank.c
+++ pagerank.c
@@ -22,6 +22,8 @@ struct URLSet {
     HashMap outgoing;
     double pagerank;
     double newPagerank;
+	double wInFactor;
+	double wOutFactor;
 };
 
 typedef struct URLSet * URLSet;
@@ -33,6 +35,8 @@ static URLSet newURLSet()
     set->outgoing = newStringKeyHashMap();
     set->pagerank = 0;
     set->newPagerank = 0;    
+	set->wInFactor = 0;
+	set->wOutFactor = 0;
     return set;
 }
 
@@ -40,9 +44,11 @@ int main (int argc, char *argv[])
 {
 	assert(argc == 4);
 	
-	//double d = atof(argv[1]);
-	//double diffPR = atof(argv[2]);
-	//int maxIterations = atoi(argv[3]);
+	double d = atof(argv[1]);
+	double diffPR = atof(argv[2]);
+	int maxIterations = atoi(argv[3]);
+	
+	printf("Max iterations: %d\n", maxIterations);
 	
 	HashMap sets = newStringKeyHashMap();
 	URLSet set;
@@ -63,10 +69,11 @@ int main (int argc, char *argv[])
 	
 	char * filename = malloc(sizeof(char) * 50);
 	char * outgoingURL = malloc(sizeof(char) * 20);
-	URLSet outgoingSet;
+	URLSet outgoingSet, incomingSet;
 	
-	int i;
+	int i, j;
 	
+	// Fill sets
 	for(i = 0; i < sizeOfHashMap(sets); i++) {
 		
 		url = urls[i];
@@ -82,8 +89,6 @@ int main (int argc, char *argv[])
             if (strstr(outgoingURL, "#end") != NULL) break;
             if (strstr(outgoingURL, "url") == NULL) continue;
 			
-			printf("URL: %s\n", outgoingURL);
-			
 			outgoingSet = getFromHashMap(sets, outgoingURL);
             putInHashMap(set->outgoing, outgoingURL, outgoingSet);
             putInHashMap(outgoingSet->incoming, url, set);
@@ -91,33 +96,100 @@ int main (int argc, char *argv[])
 			outgoingURL = malloc(sizeof(char) * 20);
         }
 		
-		set->newPagerank = 1.0 / sizeOfHashMap(sets); 
+		set->pagerank = 1.0 / sizeOfHashMap(sets);
+		//set->newPagerank = 1.0 / sizeOfHashMap(sets); 
 		
         fclose(fp);
-	} 
+	}
 	
-	/*
+	char ** outgoingUrls;
+	char ** incomingUrls;
+	
+	// Calculate wInFactor and wOutFactor
+	for(i = 0; i < sizeOfHashMap(sets); i++) {
+		
+		url = urls[i];
+		set = (URLSet) getFromHashMap(sets, url);
+		
+		outgoingUrls = malloc(sizeof(char) * sizeOfHashMap(set->outgoing));
+		getKeySetOfHashMap(set->outgoing, (void **) outgoingUrls);
+		
+		for(j = 0; j < sizeOfHashMap(set->outgoing); j++) {
+			outgoingSet = getFromHashMap(set->outgoing, outgoingUrls[j]);
+			
+			set->wInFactor += sizeOfHashMap(outgoingSet->incoming);
+			set->wOutFactor += sizeOfHashMap(outgoingSet->outgoing);
+		}
+		
+		printf("wInFactor for %s: %f\n", url, set->wInFactor);
+		printf("wOutFactor for %s: %f\n", url, set->wOutFactor);
+		
+		if(set->wInFactor > 0) set->wInFactor = 1.0 / set->wInFactor;
+		if(set->wOutFactor > 0) set->wOutFactor = 1.0 / set->wOutFactor;
+		
+		
+		
+		//free(outgoingUrls);
+	}
+	
 	double dampShift = (1 - d) / sizeOfHashMap(sets);
 	
-	int diff = difPR;
+	double diff = diffPR;
 	int iteration = 0;
-	int j, sum;
+	int sum, wIn, wOut;
 	
+	// Weighted Page Rank
 	while(iteration < maxIterations && diff >= diffPR) {
+		
+		
+		
+		for(i = 0; i < sizeOfHashMap(sets); i++) {
+			
+			url = urls[i];
+			set = (URLSet) getFromHashMap(sets, url);
+			
+			incomingUrls = malloc(sizeof(char) * sizeOfHashMap(set->incoming));
+			getKeySetOfHashMap(set->incoming, (void **) incomingUrls);
+			
+			sum = 0;
+			wIn = 0;
+			wOut = 0;
+			
+			for(j = 0; j < sizeOfHashMap(set->incoming); j++) {
+				
+				incomingSet = getFromHashMap(set->incoming, incomingUrls[j]);
+				
+				wIn = sizeOfHashMap(set->incoming) * incomingSet->wInFactor;
+				wOut = sizeOfHashMap(set->outgoing) * incomingSet->wOutFactor;
+				
+				sum += incomingSet->pagerank * wIn * wOut;
+			}
+			
+			set->newPagerank = dampShift + d * sum;
+			
+			printf("New pagerank for %s: %f\n", url, set->newPagerank);
+			
+			//free(incomingUrls);
+		}
+		
+		diff = 0;
 		
 		for(i = 0; i < sizeOfHashMap(sets); i++) {
 			url = urls[i];
 			set = (URLSet) getFromHashMap(sets, url);
+			
+			diff += abs(set->newPagerank - set->pagerank);
 			set->pagerank = set->newPagerank;
-			
-			sum = 0;
-			
-			for(j = 0; j <) //HERE
 		}
 		
 		iteration++;
 	}
-	*/
+	
+	for(i = 0; i < sizeOfHashMap(sets); i++) {
+		url = urls[i];
+		set = (URLSet) getFromHashMap(sets, url);
+		printf("Page rank of %s: %f\n", url, set->pagerank);
+	}
 	
     return 0;
 }
